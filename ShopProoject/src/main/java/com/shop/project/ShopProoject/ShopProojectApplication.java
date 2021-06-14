fe1ec692c08d321dae1e03922552c5641e3de8c0
Martin Valchev <martinvalchev1@gmail.com>
2021-06-14 22:04:31 +0300

    Initial comit

---
+++ ShopProoject/src/main/java/com/shop/project/ShopProoject/ShopProojectApplication.java
@@ -0,0 +1,13 @@
+package com.shop.project.ShopProoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopProojectApplication.class, args);
+	}
+
+}
---
+++ ShopProoject/src/test/java/com/shop/project/ShopProoject/ShopProojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.project.ShopProoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopProojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
